f48ea278c0029558451fcdf4b4ce0af33bfc8506
Sheidir <heidi.smith@students.svu.edu>
2018-04-07 09:45:16 -0400

    Fixed typo

--- src/ptn/Funct.java
+++ src/ptn/Funct.java
@@ -97,7 +97,7 @@ public RetState getReturn(){
         f.setName(getName().toAST());
         f.setType(getType().toAST());
         f.setScope(getScope());
-        f.setReturn(getReturn().toAST());
+        f.setR(getReturn().toAST());
         if (getParamList() != null) {
             f.setParams(getParamList().toParamList());
         }
